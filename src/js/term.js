8508bd262df7f3c91f67abd2408bed4eac7a59d9
QAEZZ <russian.devv@gmail.com>
2023-01-23 14:15:55 -0600

    added mystery file

--- src/js/term.js
+++ src/js/term.js
@@ -1,4 +1,4 @@
-const ls = 'Directory of C:/Home\nDATE_  TIME_  SIZE\n01/23  14:00  0000  .\n01/23  14:00  0000  ..\n01/23  14:03  0002  file1.TXT\n01/23  14:06  0016  file2.TXT\n0.005024s  ans=0x00000007=7';
+const ls = 'Directory of C:/Home\nDATE_  TIME_  SIZE\n01/23  14:00  0000  .\n01/23  14:00  0000  ..\n01/23  14:03  0002  file1.TXT\n01/23  14:06  0016  file2.TXT\n??/??  ??:??  0427  runme.HC\n0.005024s  ans=0x00000007=7';
 
 $('body').terminal({
     "Dir;": function() {
