1a6dde19f843f50ed60cb271d26dc5d2b8a0f9f7
QAEZZ <russian.devv@gmail.com>
2023-01-25 09:05:38 -0600

    updated funcs, updated `Dir;` return

--- src/js/term.js
+++ src/js/term.js
@@ -1,8 +1,10 @@
-const ls = 'Directory of C:/Home\nDATE_  TIME_  SIZE\n01/23  14:00  0000  .\n01/23  14:00  0000  ..\n01/23  14:03  0030  invite.TXT\n01/24  09:14  0132  aboutus.TXT\n01/23  14:00  013E  MakeHome.HC.Z\n01/23  14:00  01D3  Registry.HC.Z\n0.005024s  ans=0x00000007=7';
+const ls = 'Directory of C:/Home\nDATE_  TIME_  SIZE\n01/23  14:00  0000  .\n01/23  14:00  0000  ..\n01/23  14:00  013E  MakeHome.HC.Z\n01/23  14:00  01D3  Registry.HC.Z\n01/24  09:14  0132  AboutUs.TXT\n01/25  08:45  0050  GitHubOrg.TXT\n01/23  14:03  0030  Invite.TXT\n01/25  08:46  0091  ThisRepo.TXT\n0.012000s  ans=0x0000000A=10';
 const aboutus ='We are THE Holy Church of Terry Davis.\nHere at THCoTD we:\n    - Shitpost\n    - Worship Terry\n    - Do community coding projects';
 const invite = 'https://discord.gg/xEFtNzEzE8';
 const makehome = 'Cd(__DIR__);;\n//If these are not present in /Home, it uses the version in the root dir. You\n//can make your own, modified, version of these files in your /Home directory.\n#include "~/HomeLocalize"\n#include "/Adam/Opt/Boot/MakeBoot"\n#include "/Adam/Opt/Utils/MakeUtils"\n#include "~/HomeWrappers"\nMapFileLoad("::/Kernel/Kernel");\nMapFileLoad("::/Compiler/Compiler");\n#include "~/HomeKeyPlugIns"\n#include "~/HomeSys"\nCd("..");;\n\n0.000211s ans=0x00000001=1';
 const registry = '+] Adam\n+] Once\n\n0.000310s ans=0x00000001=1';
+const ghorg = 'https://github.com/The-Holy-Church-of-Terry-Davis';
+const thisrepo = 'https://github.com/The-Holy-Church-of-Terry-Davis/The-Holy-Church-of-Terry-Davis.github.io';
 
 $('body').terminal({
     "Dir;": function() {
@@ -23,10 +25,10 @@ $('body').terminal({
     "$CL$": function() {
         this.clear();
     },
-    'Type("aboutus.TXT");': function() {
+    'Type("AboutUs.TXT");': function() {
         this.echo(aboutus);
     },
-    'Type("invite.TXT");': function() {
+    'Type("Invite.TXT");': function() {
         this.echo(invite);
     },
     'Type("MakeHome.HC.Z");': function() {
@@ -34,6 +36,12 @@ $('body').terminal({
     },
     'Type("Registry.HC.Z");': function() {
         this.echo(registry);
+    },
+    'Type("GitHubOrg.TXT");': function() {
+        this.echo(ghorg);
+    },
+    'Type("ThisRepo.TXT");': function() {
+        this.echo(thisrepo);
     },
     'Cd(".");': function() {
         this.echo("0.000041s ans=0x00000001=1");
